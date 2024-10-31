11819bafed816658f3c2293866ab495c50f1f1cc
JMRO <jmro@jmro.es>
2024-10-31 09:10:55 +0000

    tercer commit

--- examen.php
+++ examen.php
@@ -10,6 +10,6 @@
     <h1><?php echo 'Juanma Rodriguez Orihuela' ?></h1>
     <h2><?php echo '31/10/2024' ?></h2>
     <h3><?php echo 'Esto es un repositorio de código' ?></h3>
-     <p><?php echo 'Esta modificacion se hizo desde vs codium' ?></p>   
+     <p><?php echo 'Esta modificacion se hizo desde vs codium' ?></p>
 </body>
 </html>
