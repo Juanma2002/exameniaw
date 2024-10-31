cb56a58b76e9bd6be0d48d8090b71025bddb99f6
JMRO <jmro@jmro.es>
2024-10-31 08:56:19 +0000

    segundo commit commit

--- examen.php
+++ examen.php
@@ -1,2 +1,15 @@
-<?php echo 'Juanma Rodriguez Orihuela-31/10/2024' ?>
-<?php echo 'Esto es un repositorio de código' ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo 'Juanma Rodriguez Orihuela' ?></h1>
+    <h2><?php echo '31/10/2024' ?></h2>
+    <h3><?php echo 'Esto es un repositorio de código' ?></h3>
+     <p><?php echo 'Esta modificacion se hizo desde vs codium' ?></p>   
+</body>
+</html>
